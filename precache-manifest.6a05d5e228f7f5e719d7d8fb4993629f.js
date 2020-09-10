f86f7319660af847ce2e59540d211ac680c7c082
sayantancha <sayantanchakraborty165.com>
2020-09-10 16:03:43 +0530

    Updates

---
+++ precache-manifest.6a05d5e228f7f5e719d7d8fb4993629f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766855a82b4ecd3b53b2c31c26e5d7fb",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "d4c8172a46e7cb2b6207",
+    "url": "/reactlive/static/css/main.bb74746a.chunk.css"
+  },
+  {
+    "revision": "6ecee77202a33ee2e68f",
+    "url": "/reactlive/static/js/2.06184ca6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactlive/static/js/2.06184ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c8172a46e7cb2b6207",
+    "url": "/reactlive/static/js/main.3316c9fa.chunk.js"
+  },
+  {
+    "revision": "fecab98bb731c7580569",
+    "url": "/reactlive/static/js/runtime-main.3d405ea4.js"
+  }
+]);
